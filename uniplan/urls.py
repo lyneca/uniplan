5fa8079f18c53f43ccb610efcfb9eca840f2d17e
Lyneca <atraguliauniliantauthr@gmail.com>
2017-12-13 10:22:48 +1100

    fixed broken url

--- uniplan/urls.py
+++ uniplan/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('planner.urls')),
+    path('planner/', include('planner.urls')),
     path('admin/', admin.site.urls),
 ]
