74e63aa4cd4bc5f1137f7452c8a5ac790b614af3
Lyneca <atraguliauniliantauthr@gmail.com>
2017-12-13 09:11:19 +1100

    added basic styling

--- planner/admin.py
+++ planner/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import User, Unit, Assessment
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email')
+from .models import Profile, Unit, Assessment
 
 class UnitAdmin(admin.ModelAdmin):
     list_display = ('name', 'long_name')
@@ -10,6 +7,6 @@ class UnitAdmin(admin.ModelAdmin):
 class AssessmentAdmin(admin.ModelAdmin):
     list_display = ('name', 'date')
 
-admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
 admin.site.register(Unit, UnitAdmin)
 admin.site.register(Assessment, AssessmentAdmin)
--- planner/models.py
+++ planner/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Unit(models.Model):
     name = models.CharField(max_length=8)
@@ -9,13 +10,21 @@ class Unit(models.Model):
     def __str__(self):
         return self.name
 
-class User(models.Model):
-    username = models.CharField(max_length=30)
-    email = models.CharField(max_length=256)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     subjects = models.ManyToManyField(Unit)
 
     def __str__(self):
-        return self.username
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Assessment(models.Model):
     name = models.CharField(max_length=100)
